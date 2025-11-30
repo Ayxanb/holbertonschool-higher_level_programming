060e879a4bbed1c4a982be0053f8d65260e20473
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-11-30 10:06:46 +0000

    task

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,10 @@
-#!/usr/bin/python3
-print(('{}'*26).format(*(chr(i) \
-        if (i%2) == 0 else chr(i-32) \
-        for i in range(122, 96, -1))),
-        sep='',
-        end='')
+#!/usr/bin/env python3
+print(
+    ('{}' * 26).format(
+        *(
+            chr(i) if (i % 2) == 0 else chr(i - 32)
+            for i in range(122, 96, -1)
+        )
+    ),
+    end=''
+)
