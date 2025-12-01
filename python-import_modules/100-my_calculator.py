61281ec80d7db40e24d4ef971f92169cc64c7c88
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-01 14:07:47 +0000

    task

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -2,13 +2,14 @@
 from calculator_1 import add, sub, mul, div
 import sys
 
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     a = int(sys.argv[1])
-    op = sys.argv[2])
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
     if op == "+":
