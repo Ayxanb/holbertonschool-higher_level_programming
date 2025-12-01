d5c41a26c0c9233c0c2ecd0bc5e3fcf036cca0b6
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-01 13:04:28 +0000

    task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-
 import sys
+
 
 if __name__ == '__main__':
     size = len(sys.argv) - 1
-    print('{} argument{}'.format(size, 's:' if size != 1 and size != 0 else 's.' if size == 0 else ':'))
+    print(
+        '{} argument{}'.format(
+            size,
+            's:' if size != 1 and size != 0 else 's.' if size == 0 else ':'
+        )
+    )
 
     for i, arg in enumerate(sys.argv[1:]):
-        print("{}: {}".format(i+1, arg))
+        print("{}: {}".format(i + 1, arg))
