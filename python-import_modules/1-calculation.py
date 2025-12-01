1c8c644d98d8cdf8e0c44063de27d7f6275112f7
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-01 12:48:39 +0000

    task

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
+if __name__ == '__main__':
+    a = 10
+    b = 5
 
-print("""{} + {} = {}
+    print("""{} + {} = {}
 {} - {} = {}
 {} * {} = {}
 {} / {} = {}""".format(
-    a, b, add(a, b), a, b, sub(a, b), a, b, mul(a, b), a, b, div(a, b)
+        a, b, add(a, b), a, b, sub(a, b), a, b, mul(a, b), a, b, div(a, b)
+        )
     )
-)
