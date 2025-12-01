273998a71f933464d4f889ffd775ba7f2bc65487
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-01 14:54:39 +0000

    task

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,5 +3,5 @@
 def print_matrix_integer(matrix):
     for list_ in matrix:
         for e in list_:
-            print("{:d}".format(e), end='')
+            print("{:d}".format(e), end=' ')
         print()
