65b46b16365b89dcafb448c5a9d7ba0984af29f3
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-01 12:58:35 +0000

    task

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,7 +3,8 @@
 import sys
 
 if __name__ == '__main__':
-    print('{} arguments'.format(len(sys.argv) - 1))
+    size = len(sys.argv) - 1
+    print('{} argument{}'.format(size, 's:' if size != 1 and size != 0 else 's.' if size == 0 else ':'))
 
     for i, arg in enumerate(sys.argv[1:]):
         print("{}: {}".format(i+1, arg))
