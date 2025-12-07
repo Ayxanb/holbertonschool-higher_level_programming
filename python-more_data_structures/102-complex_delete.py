2c988ff7ae6abc354816dfab67e09419caf665c9
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-07 10:20:02 +0000

    task

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 def complex_delete(a_dictionary, value):
+    if a_dictionary is None:
+        return a_dictionary
+    if a_dictionary.get(value) is None:
+        return a_dictionary
     for key in list(a_dictionary.keys()):
         if a_dictionary[key] == value:
             del a_dictionary[key]
