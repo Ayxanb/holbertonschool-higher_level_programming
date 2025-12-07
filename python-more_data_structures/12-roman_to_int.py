217c3002cd6fb9451e42504187bfc5b8a1f4bb36
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-07 09:51:04 +0000

    task

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -6,9 +6,19 @@ d = {
     'X': 10,
     'L': 50,
     'C': 100,
-    'D': 500
+    'D': 500,
+    'M': 1000
 }
 
+
 def roman_to_int(roman_string):
-    if type(roman_string) is not str: return None
-    return sum(map(lambda x: d[x], roman_string))
+    if (type(roman_string) is not str) or (roman_string is None):
+        return 0
+    total = 0
+    length = len(roman_string)
+    for i in range(length):
+        current_value = d[roman_string[i]]
+        total += -current_value\
+            if (i < length - 1) and (current_value < d[roman_string[i + 1]])\
+            else current_value
+    return total
