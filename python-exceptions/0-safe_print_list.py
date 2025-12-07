a4fc5943e177b3dd3bdaae0041f4e3aa99faf08c
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-07 10:33:16 +0000

    task

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list, x):
         for i in range(x):
             print(my_list[i], end='')
             printed += 1
-    except:
+    except Exception as e:
         pass
     finally:
         print()
