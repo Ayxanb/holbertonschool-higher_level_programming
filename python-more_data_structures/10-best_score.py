dd5f9299cb902b901ea9d25eed17fc2b7c7e7e71
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-07 09:26:20 +0000

    task

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    max_key = None
+    max_value = 0
+    for k, v in a_dictionary.items():
+        if v > max_value:
+            max_value = v
+            max_key = k
+
+    return max_key
