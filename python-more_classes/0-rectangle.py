f9fafbc5fb53dc4150f9115b1390df268b69434f
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-08 13:51:36 +0000

    task

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
 
+'''
+This module provides a Rectangle class
+'''
+
 class Rectangle:
+    '''
+    Rectangle class definition
+    '''
     def __init__(self):
+        """
+        (9)_(9)
+        """
         pass
