d19a35fce2c36a5a82c42a1da02de09a27ddaa3f
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-11 12:19:23 +0000

    task

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+'''I have docstrings...'''
+
+
+class BaseGeometry:
+    '''I hate docstrings...'''
+
+    def area(self):
+        raise NotImplemented("area() is not implemented")
