14710b4f98c77633f39064a8363d68f37be604a7
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-14 13:58:37 +0000

    task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -7,6 +7,9 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+with open('add_item.json', 'w') as f:
+    pass
+
 items = load_from_json_file('add_item.json')
 items.extend(sys.argv[1:])
 
