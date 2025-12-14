3e97e2946ec09a4794cc5719897fda0a5afbda1f
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-14 14:00:28 +0000

    task

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
-
-''' I hate docstring '''
-
 import sys
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-with open('add_item.json', 'w') as f:
-    pass
+filename = "add_item.json"
+with open(filename, 'w') as f: pass
+items = load_from_json_file(filename)
 
-items = load_from_json_file('add_item.json')
 items.extend(sys.argv[1:])
 
-save_to_json_file(items, 'add_item.json')
+save_to_json_file(items, filename)
