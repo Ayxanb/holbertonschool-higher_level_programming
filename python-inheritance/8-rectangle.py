41fe298140795aa95580e93d2170a061686ccab7
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-14 12:51:27 +0000

    task

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
+''' I hate writing docstr '''
+
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
+    ''' I hate writing docstr '''
     def __init__(self, width, height):
+        ''' I hate writing docstr '''
         integer_validator('width', width)
         integer_validator('height', height)
 
