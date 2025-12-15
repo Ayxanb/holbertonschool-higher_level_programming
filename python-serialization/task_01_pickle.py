35cd5ba5270d17a21e379f8a93e70bf4afc68702
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-15 13:23:20 +0000

    task

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+''' I hate docstring '''
+
+import pickle
+
+class CustomObject:
+    ''' I hate docstring '''
+
+
+    def __init__(self, name, age, is_student):
+        ''' I hate docstring '''
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+
+    def display(self):
+        ''' I hate docstring '''
+        print(f"Name: {self.name}\nAge: {self.age}\nIs Student: {self.is_student}")
+
+
+    def serialize(self, filename):
+        ''' I hate docstring '''
+        with open(filename, 'wb') as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        ''' I hate docstring '''
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
