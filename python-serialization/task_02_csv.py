f3825b08c5e31c9b258de6d475975ef9021a7a1b
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-15 13:35:09 +0000

    task

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,11 +10,19 @@ def convert_csv_to_json(filename):
     ''' I hate docstring '''
 
     data = []
-    
-    with open(filename, mode='r') as csv_file:
-        csv_reader = csv.DictReader(csv_file)
-        for row in csv_reader:
-            data.append(row)
 
-    with open('data.json', 'w') as json_file:
-        json.dump(data, json_file)
+    try:
+        with open(filename, mode='r') as csv_file:
+            csv_reader = csv.DictReader(csv_file)
+            for row in csv_reader:
+                data.append(row)
+
+        with open('data.json', 'w') as json_file:
+            json.dump(data, json_file)
+
+
+    except Exception as e:
+        return False
+
+    finally:
+        return True
