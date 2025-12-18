de42b564c70e07a9fabeae2f44408f1658718380
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-18 10:13:56 +0000

    task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -4,3 +4,7 @@ IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Grant all privileges on the server
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+
+
+-- Apply changes
+FLUSH PRIVILEGES;
