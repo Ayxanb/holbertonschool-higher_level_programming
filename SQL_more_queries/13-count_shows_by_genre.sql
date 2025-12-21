b868d87793efa4c34d16fed1c136cd67e2610549
Ayxanb <ayxanbeydiyev476@gmail.com>
2025-12-21 14:30:25 +0000

    task

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,5 @@
 -- this is a comment
-SELECT genres.name AS genre, COUNT(tv_show_genres.tv_show_id) AS number_of_shows
-FROM genres
-JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
-GROUP BY genres.id
-HAVING number_of_shows > 0
+SELECT tv_show_genres.genre_id AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
+FROM tv_show_genres
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
